a8dca81c0f923c9655379339d211aee85a8ad9e2
Lanssar <lanssar.niangane@outlook.fr>
2024-11-19 15:29:38 +0100

    Début JS

---
+++ js/test.js
@@ -0,0 +1,12 @@
+//console.log("hello world");
+
+
+//let userName = "Lanssar Niangane"; //string
+//let age = 24; //int
+//let size = 1.79; //float
+//let isCool= true; //bool
+
+let a = 23
+let b = 45
+
+console.log(a+b, a/b, a-b, a*b, a%b)
